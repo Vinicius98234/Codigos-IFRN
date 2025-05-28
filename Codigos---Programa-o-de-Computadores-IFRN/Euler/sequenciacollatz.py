993f610a42233a669b8f18e954573b2311f54056
Vinicius98234 <pollyanatrader@gmail.com>
2025-05-28 16:45:05 -0300

    ok

--- Codigos---Programa-o-de-Computadores-IFRN/Euler/sequenciacollatz.py
+++ Codigos---Programa-o-de-Computadores-IFRN/Euler/sequenciacollatz.py
@@ -3,7 +3,7 @@ print("Maior sequência de Collatz - Problema 14")
 # n = int(input("Numero qualquer para descobrir a maior sequencia = "))
 
 x = 1
-while x < 1000:
+while x < 10:
     n = x
     print(n)
     while n > 1:
@@ -12,7 +12,9 @@ while x < 1000:
             
         else: 
             n = 3 * n + 1
-        print(n)      
+        print("->", n, end ="")
+    print()
     x += 1
+print("FIM")
 
 
