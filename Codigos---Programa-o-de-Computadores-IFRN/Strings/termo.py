90b8d7eda8c9b3158b2d05fbe090918044a9ca15
Vinicius <150065293+Vinicius98234@users.noreply.github.com>
2025-06-17 17:43:51 -0300

    Create termo.py

---
+++ Codigos---Programa-o-de-Computadores-IFRN/Strings/termo.py
@@ -0,0 +1,18 @@
+import random
+palavras = ("Abacate", "Lima", "Abacaxi")
+segredo = palavras [random.randint(0,2)]
+segredo = segredo.lower()
+visivel = "_" * len(segredo)
+
+while visivel != segredo:
+    print (visivel)
+    letra = input("Diga uma letra: ")
+    novavisual = ""
+    for pos in range(len(segredo)):
+        if letra == segredo[pos]:
+            novavisual += segredo[pos]
+        else:
+            novavisual += visivel[pos]
+    visivel = novavisual
+else:
+    print("Você achou a palavra ela era:", segredo)
