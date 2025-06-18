f0ffb89e0edfba830c94bb22676bece123341e08
Vinicius98234 <pollyanatrader@gmail.com>
2025-06-17 23:06:11 -0300

    ok

--- Codigos---Programa-o-de-Computadores-IFRN/Strings/termo.py
+++ Codigos---Programa-o-de-Computadores-IFRN/Strings/termo.py
@@ -1,18 +1,45 @@
 import random
-palavras = ("Abacate", "Lima", "Abacaxi")
+
+palavras = ("Abacate", "Lima", "Abacaxi", "limao")
 segredo = palavras [random.randint(0,len(palavras)-1)]
-segredo = segredo.lower()
+segredo = segredo.upper()
 visivel = "_" * len(segredo)
+tentativas = 0
 
 while visivel != segredo:
-    print (visivel)
-    letra = input("Diga uma letra: ")
-    novavisual = ""
-    for pos in range(len(segredo)):
-        if letra == segredo[pos]:
-            novavisual += segredo[pos]
+    if tentativas <= 4:
+        print (visivel)
+        letra = input("Diga uma letra: ")
+        letra = letra.upper()
+        if len(letra) < 2:
+            for l in visivel:
+                if l == letra:
+                    print("Você já disse essa letra!")
+                    print("Tente novamente!")
+                    break
+            else:
+                novovisual = ""
+                for pos in range(len(segredo)):
+                    if letra == segredo[pos]:
+                        novovisual += segredo[pos]
+                    else:
+                        novovisual += visivel[pos]
+                visivel = novovisual
+                tentativas += 1
         else:
-            novavisual += visivel[pos]
-    visivel = novavisual
+            print("Digite apenas uma letra por vez!!")
+            print("Tente novamente!")
+    else:
+        print("Acabou o numero de chances")
+        print("Você tem mais uma chace de acertar a palavra de uma vez!")
+
+        final = input("Palavra que você suspeita que é = ")
+        final = final.upper()
+        if final == segredo:
+            print("Você acertou a chance de ouro!")
+            break
+        else:
+            print("Você é burro! A palavra era:", segredo)
+            break
 else:
     print("Você achou a palavra ela era:", segredo)
