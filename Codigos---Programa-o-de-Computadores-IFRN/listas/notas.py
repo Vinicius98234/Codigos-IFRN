ea0bb90c8fa3b442ab6f23834c934e7fe7ecc757
Vinicius98234 <pollyanatrader@gmail.com>
2025-07-01 17:41:17 -0300

    ok

--- Codigos---Programa-o-de-Computadores-IFRN/listas/notas.py
+++ Codigos---Programa-o-de-Computadores-IFRN/listas/notas.py
@@ -7,6 +7,9 @@ lstNomes = ['Scooby-Doo'       , 'Fred Flintstone', 'Zé Colmeia' , 'Dom Pixote'
             'Penélope Charmosa', 'Pepe Legal'     , 'Catatau'    , 'Dick Vigarista' ]
 
 lstnotas = []
+medias = []
+aprovados = []
+reprovados = []
 
 for n in range (len(lstNomes)):
     lstnotas.append([random.randint(0,100) ,random.randint(0,100) ,random.randint(0,100) ])
@@ -15,13 +18,14 @@ for n in range (len(lstNomes)):
 for pos in range(len(lstNomes)):
     print (lstNomes[pos], lstnotas[pos], end=" ")
     notas = lstnotas[pos]
-    nota1 = notas[0]
-    nota2 = notas[1]
-    soma = nota1 + nota2
-    media = soma / 2
-    if media >= 60:
-        print(f"\033[32mAprovado com média: {media}\033[0m")
-    else:
-        print(f"\033[31mReprovado com média: {media}\033[0m")
     
-        
+    medias.append(((notas[0] * 2) + (notas[1] * 3)) / 5)
+    if medias[pos] >= 60:
+        aprovados.append(lstNomes[pos])
+        print(f"\033[32mAprovado\033[0m com média: {medias[pos]}")
+    else:
+        reprovados.append(lstNomes[pos])
+        print(f"\033[31mReprovado\033[0m com média: {medias[pos]}")
+    
+print(f"Os alunos Aprovados são \033[32m{aprovados}\033[0m")
+print(f"Os alunos Reprovados são \033[31m{reprovados}\033[0m")
