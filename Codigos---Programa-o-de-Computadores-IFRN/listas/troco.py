7e031a8811d313ef6d66807737b63de714dd765e
Vinicius <150065293+Vinicius98234@users.noreply.github.com>
2025-07-02 14:35:06 -0300

    Update troco.py

--- Codigos---Programa-o-de-Computadores-IFRN/listas/troco.py
+++ Codigos---Programa-o-de-Computadores-IFRN/listas/troco.py
@@ -1,10 +1,12 @@
 print ("Esse programa mostra cédulas de um troco")
-produtos = ["laranja", "banana", "abacaxi", "uva", "melancia"]
-preco_produto = [2.50, 1.20, 3.00, 4.50, 5.00]
+produtos = ["Hambúrguer","Pizza","Refrigerante","Batata Frita","Salada","Suco Natural","Sorvete"]
+
+preco_produto = [15.00, 30.00,5.00,10.00, 12.00,8.00,7.00]
+
 
 for pos in range(len(produtos)):
     print(produtos[pos], preco_produto[pos])
-produto_escolhido = int(input("Escolha o produto (0 a 4): "))
+produto_escolhido = int(input("Escolha o produto (0 a 6): "))
 valor_pago = float(input("Quanto você pagou? R$ "))
 troco = valor_pago - preco_produto[produto_escolhido]
 print(f"Seu troco é de R$ {troco:.2f}")
