69c88aed4f2843b49708407be18044498096c8b3
Vinicius98234 <pollyanatrader@gmail.com>
2025-07-14 21:54:49 -0300

    ok

--- Codigos---Programa-o-de-Computadores-IFRN/Aulas/Dicionario/atletas.py
+++ Codigos---Programa-o-de-Computadores-IFRN/Aulas/Dicionario/atletas.py
@@ -3,13 +3,20 @@ import requests, json
 try:
     dados = requests.get("https://api.cartola.globo.com/atletas/mercado").text
     atletas = json.loads(dados)["atletas"]
+    posicoes = json.loads(dados)["posicoes"]
     
-    for atleta in atletas:
+    """for atleta in atletas:
         if atleta["clube_id"] == 263: # Botafogo/RJ
-            print (f"{atleta['apelido']} -> {atleta['nome']}")
+            print (f"{atleta['apelido']} -> {atleta['nome']}")"""
     
-    botafogo = filter (lambda x: x['clube_id'] == 263, atletas)
-    for atleta in botafogo:
-        print (f"{atleta['apelido']} -> {atleta['nome']}")
+    saopaulo = filter (lambda x: x['clube_id'] == 276, atletas)
+
+    saopaulo_ordenado = sorted(saopaulo, key=lambda x: x['preco_num'], reverse=True)
+    for atleta in saopaulo_ordenado:
+        posicoes_id = str(atleta['posicao_id'])
+        nome_posicao = posicoes[posicoes_id]['nome']
+        preco_ordenado = f"{atleta['preco_num']:.2f}"
+        print("\n")
+        print (f"Apelido: {atleta['apelido']} \n Nome: {atleta['nome']} \n Posição: {nome_posicao} \n Valor: C$ {preco_ordenado}")
 except json.decoder.JSONDecodeError as e:
     print ("Erro na conversão de JSON para dicionario")
