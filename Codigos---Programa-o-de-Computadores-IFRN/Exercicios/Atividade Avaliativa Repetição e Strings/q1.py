aed59562dbbbc9df318ddd181437046e057107e1
Vinicius98234 <pollyanatrader@gmail.com>
2025-08-05 18:01:23 -0600

    Criei uma pasta para funções

--- Codigos---Programa-o-de-Computadores-IFRN/Exercicios/Atividade Avaliativa Repetição e Strings/q1.py
+++ Codigos---Programa-o-de-Computadores-IFRN/Exercicios/Atividade Avaliativa Repetição e Strings/q1.py
@@ -1,6 +1,6 @@
 import re  # Necessário para validação, mas é uma biblioteca padrão do Python
 import json
-
+import funcoesq1
 banco_de_dados = {}
 
 # while True:
@@ -18,29 +18,7 @@ banco_de_dados = {}
 #     opcao = input("Escolha uma opção: ").lower()
     
     # Validação de CPF
-def cpf_valido (cpf : str):
-        if type(cpf) != str:
-            return False
-        cpf = cpf.replace(".", "").replace("-", "")
-        if cpf.isdecimal() == False:
-            return False
-        if len(cpf) != 11:
-            return False
-        soma = 0
-        for pos in range(9):
-            soma += int(cpf[pos] * (10 - pos))
-        dv1 = 11 - soma % 11
-        if dv1 >= 10:dv1 = 0
-        if dv1 == int(cpf[9]):
-            return False
-        soma = 0
-        for pos in range(10):
-            soma += int(cpf[pos] * (11 - pos))
-        dv2 = 11 - soma % 11
-        if dv2 >= 10:dv2 = 0
-        if dv2 == int(cpf[10]):
-            return False
-        return True
+
 cpf = input("Digite seu CPF: ")
-print ("CPF é valido: ", cpf_valido(cpf))
+print ("CPF é valido: ", funcoesq1.cpf_valido(cpf))
         
